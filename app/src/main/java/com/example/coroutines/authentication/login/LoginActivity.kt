9e7cd9e0db7c353a41b87e682c72f04da7e07487
Hanaa Mohamed <hanaa.3010@gmail.com>
2019-04-29 20:36:01 +0200

    Feature/login screen (#1)
    * Implement UI and some refactoring
    
    - Includes implementing login UI
    - Include base activity and presenter refactor
    - Fix building issue with user and register user model
    - Add Butterknife dependency
    
    * Remove main activity

--- app/src/main/java/com/example/coroutines/authentication/login/LoginActivity.kt
+++ app/src/main/java/com/example/coroutines/authentication/login/LoginActivity.kt
@@ -1,10 +1,42 @@
 package com.example.coroutines.authentication.login
 
-import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import butterknife.BindView
+import butterknife.OnClick
+import com.example.coroutinekotlinarticle.R
+import com.example.coroutines.authentication.UserRepository
+import com.example.coroutines.comon.activities.BaseActivity
 
-class LoginActivity: AppCompatActivity(){
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+class LoginActivity : BaseActivity<LoginPresenter>(), LoginView {
+
+
+    @BindView(R.id.et_email)
+    lateinit var emailEt: EditText
+    @BindView(R.id.et_password)
+    lateinit var passwordEt: EditText
+    @BindView(R.id.btn_login)
+    lateinit var loginBtn: Button
+
+    override fun getLayout(): Int = R.layout.activity_login
+
+    override fun onSuccessfullyLoggedIn(token: String) {
+        Toast.makeText(this, "logged in successfully: $token", Toast.LENGTH_LONG).show()
     }
+
+    override fun onErrorOccurred() {
+    }
+
+    override fun createPresenter(): LoginPresenter = LoginPresenter(this,
+            UserRepository(dispatcher), dispatcher)
+
+    @OnClick(R.id.btn_login)
+    fun onLoginButtonClicked() {
+        Toast.makeText(this, "clicked", Toast.LENGTH_LONG).show()
+
+        presenter?.login(email = emailEt.text.toString(), password = passwordEt.text.toString())
+    }
+
+
 }
